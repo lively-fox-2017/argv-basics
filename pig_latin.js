f1bf8944ab9b8d5fca71fa12bb5d70ad79d0d74c
nach9 <nur.achdiansyah@gmail.com>
2017-09-26 10:45:22 +0700

    done

--- pig_latin.js
+++ pig_latin.js
@@ -1,10 +1,12 @@
 'use strict'
 
+
+
 let pigLatin = (word) => {
   let firstLetter = word.charAt(0);
-  if(isPowel(firstLetter)){
+  if (isPowel(firstLetter)) {
     return word
-  }else{
+  } else {
     return `${word.substr(1)}${firstLetter}ay`;
   }
 }
@@ -16,7 +18,7 @@ let isPowel = (char) => {
 let convert = (sentence) => {
   let result = []
   let words = sentence.trim().split(/\s+/g)
-  for (let i=0;i<words.length;i++) {
+  for (let i = 0; i < words.length; i++) {
     result[i] = pigLatin(words[i])
   }
 
@@ -24,3 +26,8 @@ let convert = (sentence) => {
 }
 
 // Your CLI code here
+// console.log(process.argv.length)
+let sentenceArr = process.argv
+sentenceArr.shift()
+sentenceArr.shift()
+console.log(convert(sentenceArr.join(' ')))
