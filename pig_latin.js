39265b47d3536c9b2671292596afa785614bf19c
jasoneffendi <jasoneffendi27@gmail.com>
2017-09-26 11:04:41 +0700

    Argv Basics Done

--- pig_latin.js
+++ pig_latin.js
@@ -1,26 +1,53 @@
 'use strict'
 
-let pigLatin = (word) => {
-  let firstLetter = word.charAt(0);
-  if(isPowel(firstLetter)){
-    return word
-  }else{
-    return `${word.substr(1)}${firstLetter}ay`;
-  }
-}
-
-let isPowel = (char) => {
-  return (/^[aeiou]$/i).test(char);
-}
-
-let convert = (sentence) => {
-  let result = []
-  let words = sentence.trim().split(/\s+/g)
-  for (let i=0;i<words.length;i++) {
-    result[i] = pigLatin(words[i])
+class Piglatin {
+  constructor(word) {
+    this.hasil = this.convert(word)
   }
 
-  return result.join(" ")
+  pigLatin(kata) {
+    let firstLetter = kata.charAt(0)
+    if (this.isPowel(firstLetter)) {
+      return kata
+    } else {
+      for (var i=0; i<kata.length; i++) {
+        if (this.isPowel(kata.charAt(i)) === true) {
+          return `${kata.substr(i)}${kata.substr(0,i)}ay `
+        }
+      }
+    }
+  }
+
+  isPowel(hurufPertama) {
+    return (/^[aeiou]$/i).test(hurufPertama)
+  }
+
+  convert(sentence) {
+    let result = []
+    // this.words = sentence.trim().split(/\s+/g)
+    for (let i=0;i<sentence.length;i++) {
+      result[i] = this.pigLatin(sentence[i])
+    }
+    // for (let i=0;i<sentence.length;i++) {
+    //   result[i] = this.pigLatin(sentence[i])
+    // }
+    return result.join('')
+  }
+
 }
+
 
 // Your CLI code here
+let arr = process.argv
+let values = arr.slice(2)
+// console.log(values)
+
+let obj = {
+  word: values
+}
+console.log(obj.word)
+
+let kata = new Piglatin(obj.word)
+console.log(kata)
+
+// Add Comment
